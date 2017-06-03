0aaab06651abb908e8e1b3e34b2babf3e45cdb2a
Dylan Simowitz <dsimowitz@gmail.com>
2017-06-03 11:43:31 -0700

    Update theme

--- web/app/themes/sage/templates/partials/featured-image.blade.php
+++ web/app/themes/sage/templates/partials/featured-image.blade.php
@@ -1,14 +1,8 @@
 <div class="featured-image">
-  {{-- <div id="featured-image" style="background-image: url({{the_post_thumbnail_url()}})"></div> --}}
-  
   @if(is_page('contact'))
     <div id="featured-image" style="background-image: url(https://maps.googleapis.com/maps/api/staticmap?&zoom=12&size=640x640&scale=2&maptype=roadmap&markers=color:red%7C{{urlencode(get_field('street', 'options'))}}&key={{get_field('google_maps_key', 'options')}})"></div>
   @else
-    @if(has_post_thumbnail())
-      <div id="featured-image" style="background-image: url({{the_post_thumbnail_url()}})"></div>
-    @else
-      <div id="featured-image" style="background-image: url(https://source.unsplash.com/collection/329512/1600x900?sig={{uniqid()}})"></div>
-    @endif
+    <div id="featured-image" style="background-image: url({{the_post_thumbnail_url()}})"></div>
   @endif
     <div class="featured-image__meta">
       <h2 class="featured-image__page-title">{{the_title()}}</h2>
